362ab07972ee22ce736561884830108170ae7123
DANDI User <info@dandiarchive.org>
2026-04-22 04:47:28 +0000

    [backups2datalad] 6 files added

---
+++ derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/code/submit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=AIND-Ephys-Pipeline
+#SBATCH --output=/orcd/data/dandi/001/dandi-compute/processing/tmpt5yi689t/001697/derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/logs/job-%j_slurm.log
+#SBATCH --mem=1GB
+#SBATCH --cpus-per-task 1
+#SBATCH --partition=mit_normal
+#SBATCH --time=12:00:00
+
+NWB_FILE_PATH="/orcd/data/dandi/001/s3dandiarchive/blobs/048/d1e/048d1ee9-83b7-491f-8f02-1ca615b1d455"
+DATA_PATH="/orcd/data/dandi/001/s3dandiarchive/blobs/048/d1e"
+
+RESULTS_PATH="/orcd/data/dandi/001/dandi-compute/processing/tmpt5yi689t/001697/derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/intermediate"
+WORKDIR="/orcd/data/dandi/001/dandi-compute/work"
+NXF_APPTAINER_CACHEDIR="/orcd/data/dandi/001/dandi-compute/work/apptainer_cache"
+
+source /etc/profile.d/modules.sh
+module load miniforge
+module load apptainer
+
+conda activate /orcd/data/dandi/001/environments/name-nextflow_environment
+
+# Ensure the correct version of AIND pipeline is used
+git -C "/orcd/data/dandi/001/dandi-compute/aind-ephys-pipeline.cody" checkout v1.1.0
+
+# Need to ensure latest DANDI-CLI version is always used, otherwise upload of logs may not be possible at the end
+pip install -U dandi
+
+DATA_PATH="$DATA_PATH" RESULTS_PATH="$RESULTS_PATH" NXF_APPTAINER_CACHEDIR="$NXF_APPTAINER_CACHEDIR" nextflow \
+    -C "/orcd/data/dandi/001/dandi-compute/processing/tmpt5yi689t/001697/derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/code/mit_engaging.config" \
+    -log "/orcd/data/dandi/001/dandi-compute/processing/tmpt5yi689t/001697/derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/logs/nextflow.log" \
+    run "/orcd/data/dandi/001/dandi-compute/aind-ephys-pipeline.cody/pipeline/main_multi_backend.nf" \
+    -work-dir "$WORKDIR" \
+    --params_file "/orcd/data/dandi/001/dandi-compute/processing/tmpt5yi689t/001697/derivatives/dandiset-214527/sub-test/ses-aind+sample/pipeline-aind+ephys/version-v1.1.0+c75f900/params-98fd947_config-6568dda_date-2026+04+22_attempt-1/code/default.json" \
+    --job_dispatch_args "--nwb-files $NWB_FILE_PATH"
+
+cd $RESULTS_PATH
+mv nwb/ ../derivatives/
+mv visualization_output.json visualization/
+mv visualization/ ..
+mv nextflow/* ../logs/
+cd ..
+rm -rf $RESULTS_PATH  # Clean up intermediate values
+
+dandi upload --validation skip  # Dandiset is valid if ignoring NWBI issues from copied files (BIDS part is valid)
+echo "tmpt5yi689t" >> /orcd/data/dandi/001/dandi-compute/processing/done.txt
